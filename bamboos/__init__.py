291eb1a41ae45148b96fac2313efef980b37ea1b
adityasidharta <aditya@ucare.io>
2019-02-10 18:53:53 +0800

    Boilerplate

---
+++ bamboos/__init__.py
@@ -0,0 +1,8 @@
+"""An amazing sample package!"""
+
+__version__ = '0.1'
+
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
