8732e4bbb3b93fc519deff51214a3cbf6ecf6bda
Aditya Sidharta <aditya.sdrt@gmail.com>
2019-05-24 14:01:47 +0800

    add categorical encoders to bb functions

--- bamboos/__init__.py
+++ bamboos/__init__.py
@@ -1,5 +1,15 @@
 """An amazing sample package!"""
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from bamboos.date import date_single, date_double
+from bamboos.encode import (
+    fit_binary,
+    fit_categorical,
+    fit_label,
+    fit_onehot,
+    transform_binary,
+    transform_categorical,
+    transform_label,
+    transform_onehot,
+)
